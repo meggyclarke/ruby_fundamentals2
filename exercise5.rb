f30cace1432f06ff9ac17f4acbf7ae4819eaadb0
Meg Clarke <meggyclarke@gmail.com>
2016-10-19 14:23:32 -0400

    First attempt

---
+++ exercise5.rb
@@ -0,0 +1,12 @@
+puts "Hi There :) What is the tempeture out there in the real world? (Fereheight Please)"
+  f = gets.chomp.to_i
+
+#puts f
+
+ step_one = (f - 32)
+
+#puts step_one
+
+c =  (step_one * 1.8)
+
+ puts " #{c} Oh computers do not feel that temperature"
