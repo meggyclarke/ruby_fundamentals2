24b4dea3ec9dff500722bc1259476d5965d5044e
Meg Clarke <meggyclarke@gmail.com>
2016-10-19 15:28:20 -0400

    Improved with Round after slack suggestion

--- exercise5.rb
+++ exercise5.rb
@@ -1,5 +1,5 @@
 puts "Hi There :) What is the tempeture out there in the real world? (Fereheight Please)"
-  f = gets.chomp.to_i
+  f = gets.chomp.to_i.round(2)
 
 def temperature(temp)
   (temp - 32)* 1.8
